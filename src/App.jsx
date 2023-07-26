d2290802314ee7b99a89b445d60384cefd78b290
DaviiJones <davonbridgett@pursuit.org>
2023-07-25 21:53:31 -0400

    jalal & davon's lab 9:52pm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import NavBar from "./NavBar.jsx";
 import "./index.css";
 
 function App() {
-  return <p>Hello, world!</p>;
+  
+  return(
+  <div>
+  <p>Hello, world!</p>;
+    <NavBar/>
+
+  </div>
+  
+  ) 
+
+
 }
 
 export default App;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+return (
+<div>
+<button type="button">What Is Pursuit?</button>
+
+<button type="button">Create an Account</button>
+
+<button type="button">Sign In</button>
+</div>
+);
+
+}
