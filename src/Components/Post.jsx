2612fcf3489c8fc05613dd531b190c22cc0f276e
DaviiJones <davonbridgett@pursuit.org>
2023-07-25 22:04:16 -0400

    added singular post

---
+++ src/Components/Post.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+return (
+<div>
+<h2>Title</h2>
+<p>Lorem ipsum</p>
+</div>
+);
+
+}
--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -4,6 +4,9 @@ const NavBar = () => {
 return (
 <div>
 <img src='./src/profile.png'></img>
+<h2>This Guy</h2>
+<h3>React Developer</h3>
+<p>Loren ipsem</p>
 </div>
 );
 
